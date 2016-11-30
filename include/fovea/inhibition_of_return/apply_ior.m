64869793d499d6232a8f398cf630bdb14887df97
dberga <maildber@gmail.com>
2016-11-30 13:24:13 +0100

    v19-0-1 inhibition of return, second trial

--- include/fovea/inhibition_of_return/apply_ior.m
+++ include/fovea/inhibition_of_return/apply_ior.m
@@ -3,16 +3,18 @@ function [ out_map ] = apply_ior( map, time,iter, struct )
         %struct.gaze_params.ior_angle (rad)
         %struct.gaze_params.ior_factor_ctt (firing rate)
     
-    inhibition_factor = get_ior_factor(time,struct.zli_params.n_membr,iter,struct.zli_params.n_iter);
-        ior_factor_max = get_ior_factor(1,struct.zli_params.n_membr,1,struct.zli_params.n_iter);
-        ior_factor_min = get_ior_factor(struct.zli_params.n_membr,struct.zli_params.n_membr,struct.zli_params.n_iter,struct.zli_params.n_iter);
+    inhibition_factor = get_ior_factor(time,struct.zli_params.n_membr,iter,struct.zli_params.n_iter,struct.gaze_params.ior_slope_ctt);
+        %ior_factor_max = get_ior_factor(1,struct.zli_params.n_membr,1,struct.zli_params.n_iter,struct.gaze_params.ior_slope_ctt);
+        %ior_factor_min = get_ior_factor(struct.zli_params.n_membr,struct.zli_params.n_membr,struct.zli_params.n_iter,struct.zli_params.n_iter,struct.gaze_params.ior_slope_ctt);
         
     inhibition_gaussian = get_ior_gaussian(struct.gaze_params.fov_x, struct.gaze_params.fov_y, inhibition_factor, struct.gaze_params.ior_angle, struct.gaze_params.orig_height, struct.gaze_params.orig_width, struct.gaze_params.img_diag_angle);
         
-    inhibition = normalize_minmaxp(inhibition_gaussian,ior_factor_min,ior_factor_max); %normalize from min to max
-        inhibition= inhibition .* struct.gaze_params.ior_factor_ctt;
+    %inhibition = normalize_minmax(inhibition_gaussian); %function already normalizes
+    
+    inhibition= inhibition_gaussian  .* struct.gaze_params.ior_factor_ctt;
         
     out_map = map+inhibition;
     
+
 end
 
--- include/fovea/inhibition_of_return/try_ior.m
+++ include/fovea/inhibition_of_return/try_ior.m
@@ -1,26 +1,35 @@
 
+close all;
+
+
 struct.gaze_params.orig_width = 300;
 struct.gaze_params.orig_height = 200;
 struct.gaze_params.fov_x = round(struct.gaze_params.orig_width/2);
 struct.gaze_params.fov_y = round(struct.gaze_params.orig_height/2);
 struct.gaze_params.img_diag_angle = degtorad(35);
 struct.gaze_params.ior_factor_ctt = 2;
+struct.gaze_params.ior_slope_ctt = 1;
 struct.gaze_params.ior_angle = degtorad(4);
 struct.zli_params.n_membr = 10;
 struct.zli_params.n_iter = 10;
 map_in = zeros(200,300);
 
+
+
+figure(2);cla; hold on; for s=1:10; color = rand(1,3); plot(0.1:0.1:1,get_ior_factor(1:struct.zli_params.n_membr,struct.zli_params.n_membr,1:struct.zli_params.n_iter,struct.zli_params.n_iter,s),'color',color); end;
+
 while(true)
 for t=1:struct.zli_params.n_membr
     for i=1:struct.zli_params.n_iter
         map_out = apply_ior(map_in,t,i, struct);
-        %figure,
+        figure(1);
         pause(0.1);
         imagesc(map_out);
         colorbar;
         xlabel(['t=' num2str(t) ',' 'iter=' num2str(i)]);
+        
     end
 end
 end
-close all;
+
 
