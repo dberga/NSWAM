1e59079931aea78e7fe47f409ec9ac5af706d229
david.berga <david.berga>
2017-03-27 12:26:28 +0200

    v20-0-6 fixed dva

--- src/get_smaps_gaussian.m
+++ src/get_smaps_gaussian.m
@@ -1,6 +1,7 @@
 function [gaussian_smap] = get_smaps_gaussian(scanpath,conf_struct)
     bmap = scanpath2bmap(scanpath, [conf_struct.gaze_params.orig_height conf_struct.gaze_params.orig_width]);
-    gaussian_smap = bmap2dmap(bmap);  
+    if ~exist('conf_struct.gaze_params.dva','var'), conf_struct.gaze_params.dva=40;  end
+    gaussian_smap = zhong2012(double(bmap),dva);  
     
 end
 
