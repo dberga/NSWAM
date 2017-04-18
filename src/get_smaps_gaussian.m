87bde19ff4ffdd3f5b1655ece422315671c79a21
dberga <dberga@cvc.uab.es>
2017-04-18 21:21:18 +0200

    v20-0-19 gaussian forced to be created

--- src/get_smaps_gaussian.m
+++ src/get_smaps_gaussian.m
@@ -1,7 +1,7 @@
 function [gaussian_smap] = get_smaps_gaussian(scanpath,conf_struct)
     bmap = scanpath2bmap(scanpath, [conf_struct.gaze_params.orig_height conf_struct.gaze_params.orig_width]);
     if ~exist('conf_struct.gaze_params.dva','var'), conf_struct.gaze_params.dva=40;  end
-    gaussian_smap = zhong2012(double(bmap),conf_struct.gaze_params.dva);  
+    gaussian_smap = uint8(255*zhong2012(double(bmap),conf_struct.gaze_params.dva));  
     
 end
 
