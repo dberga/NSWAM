8621cebe98ce563c8a182a8eb68a38dad0d84681
dber <maildber@gmail.com>
2017-05-25 15:41:35 +0200

    added executa small for several configs atst

--- executa.m
+++ executa.m
@@ -16,31 +16,36 @@ end
 
 %function dependencies
 addpath('include/file_utils');
-raddpath('include');
+addpath(genpath('include'));
 
 %NCZLd - Xavi/Xim/David
 addpath('src');
 addpath('src_mex');
 
 conf_mats= dir(fullfile(conf_dir, ['*.mat']));
-%conf_mats=unsort_array(conf_mats);
+conf_mats=unsort_array(conf_mats);
 
 
  %delete(gcp);
  %parpool('local',4);
 
+mkdir('logs');
+done_name=['logs/' 'done_' conf_mats(i).name];
+
 for i=1:length(conf_mats) %parfor i=1:length(conf_mats)
     conf_path = [conf_dir '/' conf_mats(i).name];
-    disp([conf_path ':']);
-    args = {conf_path, output_dir, mats_dir, output_extension};
-	try
-    improcdir(funcio,fileformat,1,input_dir,args);
-	catch exc_general
-		fileID=fopen('errors.log','w');
-		fprintf(fileID,['Error en ' conf_mats(i).name ':' getReport(exc_general)]);
-		fclose(fileID);
-	end
-end
+    if ~exist(done_name,'file')
+	    disp([conf_path ':']);
+	    args = {conf_path, output_dir, mats_dir, output_extension};
+		%try
+		    improcdir(funcio,fileformat,1,input_dir,args);
+		    save(done_name,'done_name');
+		%catch exc_general
+		%	fileID=fopen('errors.log','w');
+		%	fprintf(fileID,['Error en ' conf_mats(i).name ':' getReport(exc_general)]);
+		%	fclose(fileID);
+		%end
+     end
 
 
 
