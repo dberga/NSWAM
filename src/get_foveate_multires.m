4ef73f612aede21b58ec904648fcc215e284ed06
dberga <dberga@cvc.uab.es>
2017-08-24 22:11:31 +0200

    foveate multires, working with monochromatic images - fix

--- src/get_foveate_multires.m
+++ src/get_foveate_multires.m
@@ -1,6 +1,6 @@
 function [ curvs, residuals ] = get_foveate_multires( curvs_aux,residuals_aux,conf_struct )
 
-    for c=1:conf_struct.color_params.nchannels
+    for c=1:numel(curvs_aux)
         for s=1:conf_struct.wave_params.n_scales-1
             for o=1:conf_struct.wave_params.n_orient
                 curvs{c}{s}(:,:,o)=get_foveate(curvs_aux{c}{s}(:,:,o),conf_struct,1);
