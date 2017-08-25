2440eaf2a3b6e8ee8100f4a4cf916a77eb42de22
dberga <dberga@cvc.uab.es>
2017-08-25 02:25:22 +0200

    slink wk fix

--- src/slink.m
+++ src/slink.m
@@ -1,7 +1,11 @@
 function [  ] = slink( path1, path2 )
 
 if exist(path2,'file') system(['rm -f' ' ' path2]); end;
-system(['ln -sf ' path1 ' ' path2]);  
+try
+	system(['ln -rsf ' path1 ' ' path2]);  
+catch
+	system(['ln -s ' path1 ' ' path2]);
+end
 
 end
 
