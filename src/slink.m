798010577f42f7d3d5821c762f533e1d4c5bcfb1
dberga <dberga@cvc.uab.es>
2017-10-09 15:57:40 +0200

    slink with all cases, if not work, use cp

--- src/slink.m
+++ src/slink.m
@@ -4,7 +4,15 @@ if exist(path2,'file') system(['rm -f' ' ' path2]); end;
 try
 	system(['ln -rsf ' path1 ' ' path2]);  
 catch
-	system(['ln -s ' path1 ' ' path2]);
+	try 
+		system(['ln -s ' path1 ' ' path2]);
+	catch
+		try 
+			system(['ln -r ' path1 ' ' path2]);
+		catch
+			system(['cp ' path1 ' ' path2]);
+		end
+	end
 end
 
 end
