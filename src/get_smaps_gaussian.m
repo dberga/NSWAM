9e931c4b1f0e76dcd9ff19077abe190ad9be9790
dberga <dberga@cvc.uab.es>
2017-11-02 19:09:00 +0100

    gaussian option as a parameter

--- src/get_smaps_gaussian.m
+++ src/get_smaps_gaussian.m
@@ -1,7 +1,10 @@
 function [gaussian_smap] = get_smaps_gaussian(scanpath,conf_struct)
     bmap = scanpath2bmap(scanpath, [conf_struct.gaze_params.orig_height conf_struct.gaze_params.orig_width]);
     params.pxva=radtodeg(conf_struct.gaze_params.img_diag_angle);
-    gaussian_smap=uint8(255*bmap2dmap(double(bmap),2,params));
+    
+    if ~isfield(conf_struct.fusion_params,'gaussian_option') conf_struct.fusion_params.gaussian_option = 2; end
+    
+    gaussian_smap=uint8(255*bmap2dmap(double(bmap),conf_struct.fusion_params.gaussian_option,params));
     
 end
 
