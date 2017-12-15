7c5ab9808e85eb372c6c7f19f361ab06088f66a0
dberga <dberga@cvc.uab.es>
2017-12-15 18:40:55 +0100

    save mat with try catch with absolute path

--- src/save_mat.m
+++ src/save_mat.m
@@ -8,7 +8,15 @@ function [] = save_mat(mat_name,matrix_in,folder_props,image_props,gaze_idx,chan
     else
         mat_path = [ folder_props.output_folder_mats '/' image_props.image_name_noext '_' mat_name '.mat'];
     end
-    save(mat_path,'matrix_in');
+    try
+        save(mat_path,'matrix_in');
+    catch
+        try
+            save([pwd '/' mat_path],'matrix_in');
+        catch
+            save(mat_path,'matrix_in','-v7.3');
+        end
+    end
 
 end
 
