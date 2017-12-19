c5e85bc1ab6f2f34d62b5e939a27d6443ba0c535
dberga <dberga@cvc.uab.es>
2017-12-19 18:31:39 +0100

    mkdir if savemat cannot create2

--- src/save_mat.m
+++ src/save_mat.m
@@ -9,10 +9,9 @@ function [] = save_mat(mat_name,matrix_in,folder_props,image_props,gaze_idx,chan
         mat_path = [ folder_props.output_folder_mats '/' image_props.image_name_noext '_' mat_name '.mat'];
     end
     try
+        mkdir(folder_props.output_folder_mats);
         save(mat_path,'matrix_in');
     catch
-	    [mat_path_folder,~,~]=fileparts(mat_path);
-	    mkdir(mat_path_folder);
         try
             save([pwd '/' mat_path],'matrix_in');
         catch
