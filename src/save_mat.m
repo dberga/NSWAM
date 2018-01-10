9f92553d52f097da14cee4e9b59b5357cae1e9fb
dberga <dberga@cvc.uab.es>
2018-01-10 12:08:45 +0100

    delete mat path before save if output exists as broken link

--- src/save_mat.m
+++ src/save_mat.m
@@ -9,24 +9,20 @@ function [] = save_mat(mat_name,matrix_in,folder_props,image_props,gaze_idx,chan
         mat_path = [ folder_props.output_folder_mats '/' image_props.image_name_noext '_' mat_name '.mat'];
     end
     try
-        mkdir(folder_props.output_folder_mats);
         save(mat_path,'matrix_in');
     catch
         try
+            %in case folder does not exist or file exists as bad link
+            mkdir(folder_props.output_folder_mats);
+            delete([pwd '/' mat_path]);
+            
+            %absolute path
             save([pwd '/' mat_path],'matrix_in');
         catch
             try
-                save(mat_path,'matrix_in','-v7.3');
+                save(['/home/media/dberga/DATA/repos/matlab' '/' mat_path],'matrix_in');
             catch
-                try 
-                    save([pwd '/' mat_path],'matrix_in','-v7.3');
-                catch
-                    try
-                        save(['/home/media/dberga/DATA/repos/matlab' '/' mat_path],'matrix_in');
-                    catch
-                        save(['/home/davidb/neuro/matlab' '/' mat_path],'matrix_in');
-                    end
-                end
+                save(['/home/davidb/neuro/matlab' '/' mat_path],'matrix_in');
             end
         end
     end
