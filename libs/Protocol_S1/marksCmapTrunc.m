efc35d7542d3e8bd9d93c706b4948b70cddbc5b8
dberga <dberga@cvc.uab.es>
2018-11-23 16:13:52 +0100

    deprecated code from other repos (ltfat and schira's code)

--- libs/Protocol_S1/marksCmapTrunc.m
+++
@@ -1,9 +0,0 @@
-function map = marksCmapTrunc(m)
-if nargin < 1, m = size(get(gcf,'colormap'),1); end
-startMap=hsv(6);
-startMap=[startMap(1:3,:);[0 0.5 0];startMap(4:5,:)];
-
-samplePoints = linspace(1,size(startMap,1), m);
-map = interp1(startMap,samplePoints);
-
-
